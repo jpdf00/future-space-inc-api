b215039c61ccd9bf313f5c18a1fbe07f7fefc7ed
João Paulo Dias França <j.pdf00@gmail.com>
2022-10-13 08:02:00 -0300

    Adds sidekiq and sidekiq-cron

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module FutureSpaceIncApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.autoload_paths << Rails.root.join('/app/services')
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
